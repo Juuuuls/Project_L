7554db37aca4335cb352cb276a6e0f80a312e37e
JackNjHill <129560233+JackNjHill@users.noreply.github.com>
2023-10-23 19:22:11 +0800

    views.py

--- Flask-Web-App-Tutorial-main/Flask-Web-App-Tutorial-main/website/views.py
+++ Flask-Web-App-Tutorial-main/Flask-Web-App-Tutorial-main/website/views.py
@@ -19,18 +19,18 @@ def main_page():
 def sms():
     if request.method == 'POST':
         phone_number = request.form.get('phone_number')
-        message = request.form.get('message')
+        message = "Help! I don't feel safe" 
 
         # Initialize Twilio client with your Account SID and Auth Token
-        account_sid = 'your_account_sid'
-        auth_token = 'your_auth_token'
+        account_sid = 'ACc4cc395fadb3626dd58cdf01d4c38f3c'
+        auth_token = 'd7b4df52435c42b39672e31a6c068622'
         client = Client(account_sid, auth_token)
 
         try:
             # Send SMS
             client.messages.create(
                 body=message,
-                from_='your_twilio_phone_number',
+                from_='+15105879524',
                 to=phone_number
             )
             flash('SMS sent successfully!', category='success')
